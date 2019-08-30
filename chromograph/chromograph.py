mikaell/chromograph

commit 16f3bdc8009ff3b342d7cf19a036f54804e5acea

Fix Pip Install
Pip install command was not working! This commit fixes the bug.
Usage:
    $ pip install -e .

chromograph/chromograph.py
@@ -22,7 +22,7 @@ import numpy as np
 import pandas
 import re
 import sys
-from utils import read_cfg, filter_dataframe, png_filename, outpath
+from .utils import read_cfg, filter_dataframe, png_filename, outpath
 
 PADDING = 200000
 HEIGHT = 1
@@ -386,3 +386,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+

setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
 
 setup(
-   name='chromograph',
-   version='0.1',
-   description='tool for plotting genetic data',
-   author='mikaell',
-   author_email='mikael.laaksonen@scilifelab.se',
-   packages=['chromograph'],  #same as name
-   install_requires=['pandas', 'matplotlib', 'numpy', 'pyaml'],
+    name='chromograph',
+    version='0.1',
+    description='tool for plotting genetic data',
+    author='mikaell',
+    author_email='mikael.laaksonen@scilifelab.se',
+    packages=find_packages(exclude=('tests*', 'docs', 'examples')),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['pandas', 'matplotlib', 'numpy', 'pyaml'],
+    entry_points=dict(
+        console_scripts=[
+            'chromograph = chromograph.chromograph:main',
+        ],
+    ),
+
 )

commit 676c1f8878300ce341da8b7bdc46ce2350f8aa22

Bugfix: Could Not Run as Script
Bugfix an error introduced in 16f3bdc8. Could not run as

    $./chromograph.py

Now this works.

chromograph/chromograph.py
@@ -22,7 +22,7 @@ import numpy as np
 import pandas
 import re
 import sys
-from .utils import read_cfg, filter_dataframe, png_filename, outpath
+from chr_utils import read_cfg, filter_dataframe, png_filename, outpath
 
 PADDING = 200000
 HEIGHT = 1