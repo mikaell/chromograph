6cf53df078e20e4908ced1d0ea780907d45b94ed
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-09-13 11:23:29 +0200

    Add fallback wig_step if cfg is not read.

--- chromograph/chr_utils.py
+++ chromograph/chr_utils.py
@@ -14,7 +14,8 @@ def read_cfg():
         'chromosome_str': ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7',
                            'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13',
                            'chr14' , 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
-                           'chr20', 'chr21', 'chr22', 'chrM', 'chrX', 'chrY']}
+                           'chr20', 'chr21', 'chr22', 'chrM', 'chrX', 'chrY'],
+        'wig_step': 5000}
 
 
 
@@ -43,4 +44,3 @@ def outpath(outd, infile, label):
     outpath = outd + "/" + outfile
     print("outfile: {}".format(outpath))
     return outpath
-
