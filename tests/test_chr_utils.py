9005b6ca534bd17637269660b759b99b733b7dff
mikaell <mikael.laaksonen@scilifelab.se>
2020-05-19 09:58:02 +0200

    Add Test Case and Pylint Changes

--- tests/test_chr_utils.py
+++ tests/test_chr_utils.py
@@ -1,7 +1,7 @@
 #
 #
 #
-import os, pathlib
+import os
 
 from chromograph.chr_utils import (chrFormat, cast, read_cfg, filter_dataframe,
                                    png_filename, outpath, parseWigDeclarationLine,
@@ -17,15 +17,15 @@ def test_chrFormat_str():
     # GIVEN a string, not representing an integer, i.e. 'chr3'
     # THEN chrFormat returns 'str'
     assert chrFormat('chr4') == 'str'
-    
+
 
 def test_makeDict():
     # GIVEN a list where each element is on format: '<char>=<int>'
-    s = ['a=1','b=2','c=3']
+    test_list = ['a=1', 'b=2', 'c=3']
     # THEN the list is transformed to a key/value dict, <char>:<int>
-    d = {'a':'1', 'b':'2', 'c':'3'}
-    # 
-    assert d == makeDict(s)
+    test_dict = {'a':'1', 'b':'2', 'c':'3'}
+    #
+    assert test_dict == makeDict(test_list)
 
 
 def test_png_filename():
@@ -33,12 +33,19 @@ def test_png_filename():
     # THEN png_filename() will return "test_myLabel.png"
     assert "test_myLabel.png" == png_filename("test.file", "myLabel")
 
-    
+
 def test_outpath(tmpdir):
     # WHEN setting up temporary directory and a file
     outd = tmpdir.mkdir("directory")
     infile = "file.test"
     # GIVEN simulated result, on format `path/to/directory/file_LABEL.png`
-    d = os.path.join(outd, "file_LABEL.png")
+    test_dirpath = os.path.join(outd, "file_LABEL.png")
     # THEN outpath will match
-    assert d == outpath(outd, infile, "LABEL")
+    assert test_dirpath == outpath(outd, infile, "LABEL")
+
+
+def test_cast():
+    # GIVEN a dict, simulated to parsing a Wig-file header
+    wigHeader = {'chrom': '1', 'start': '1', 'step': '10000\n'}
+    # THEN calling `cast` parsed values are typecast to internally used types
+    assert cast(wigHeader) == {'chrom': 'int', 'start': 1, 'step': 10000}
