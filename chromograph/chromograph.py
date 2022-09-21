c5647a805bfd61ef635fb95b4cdc11c2fdbfee32
mikaell <mikael.laaksonen@scilifelab.se>
2022-09-21 08:43:50 +0200

    Remove DPI Stuff For Clean Refactor Commit

--- chromograph/chromograph.py
+++ chromograph/chromograph.py
@@ -48,9 +48,6 @@ HELP_STR_UPD_SITE = "Plot UPD sites from bed file "
 HELP_STR_EXOM = "Plot exom coverage from bed file "
 HELP_STR_VSN = "Display program version ({}) and exit."
 
-DPI_SMALL = 100
-DPI_MEDIUM = 1000
-DPI_LARGE = 5000
 PADDING = 200000
 CHROM_END_POS = 249255000  # longest chromosome is #1, 248,956,422 base pairs
 HEIGHT = 1
@@ -121,9 +118,7 @@ DEFAULT_SETTING = {
     "combine": False,
     "normalize": False,
     "euploid": False,
-    "agg_chunk_size": 10000,
-    "dpi": DPI_MEDIUM,
-}
+    "agg_chunk_size": 10000}
 
 get_color = {
     # Cytoband colors
@@ -305,10 +300,6 @@ def _args_to_dict(filepath, args):
     if "outd" in head and head["outd"] is not None:
         _assure_dir(head["outd"])
         settings["outd"] = head["outd"]
-    if "small" in head:
-        settings["dpi"] = DPI_SMALL
-    if "large" in head:
-        settings["dpi"] = DPI_LARGE
     settings["step"] = head.get("step")
     return settings
 
@@ -941,9 +932,6 @@ def main():
         "-u", "--chunk", type=int, help="Set Matplotlib.agg.path.chunksize (default 10000)"
     )
     parser.add_argument("-x", "--combine", help=HELP_STR_COMBINE, action="store_true")
-    parser.add_argument("--small", action="store_true")
-    parser.add_argument("--medium", action="store_true")
-    parser.add_argument("--large", action="store_true")
 
     args = parser.parse_args()
     print("ARGS")
@@ -955,9 +943,6 @@ def main():
     args.norm = "norm" if args.norm else None
     args.combine = "combine" if args.combine else None
     args.euploid = "euploid" if args.euploid else None
-    args.dpi = DPI_SMALL if args.small else DPI_MEDIUM
-    args.dpi = DPI_LARGE if args.large else DPI_MEDIUM
-
     agg_chunks_size = args.chunk if args.chunk else DEFAULT_SETTING["agg_chunk_size"]
     matplotlib.rcParams["agg.path.chunksize"] = agg_chunks_size
 
